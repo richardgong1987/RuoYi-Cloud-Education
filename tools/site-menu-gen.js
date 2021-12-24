b40a0d61869104ec905bb4dc7f09b6406507a3cb
gong.hanjin <richardgong1988@gmail.com>
2021-12-24 18:26:39 +0800

    feat: added reorder menuId tool

--- tools/site-menu-gen.js
+++ tools/site-menu-gen.js
@@ -1,17 +1,22 @@
+const {reorderMenuIds, outSql} = require("./utils");
 var sql = `
 USE \`ry-cloud\`;
 DELETE
 FROM sys_menu
-WHERE menu_id >= 2000;
+WHERE menu_id >= 3000;
 
 DELETE
 FROM sys_menu
 WHERE menu_id=4;
 
 `;
-let menuData = require('toml').parse(require('fs').readFileSync('./menu/menu.toml'));
 
-genSql(menuData);
+(async function (arg) {
+    await reorderMenuIds()
+    let menuData = require('toml').parse(require('fs').readFileSync('./menu/menu.toml'));
+    genSql(menuData);
+    await outSql(sql);
+}("get started"));
 
 function genSql(menuData, M_order_num = 0, parentId = 0) {
     for (let key1 in menuData) {
@@ -63,7 +68,9 @@ function firstMenu(M, M_order_num, firstMenuKey, parentId = '0') {
     }
 
     return `
-insert into sys_menu values('${M.menuId}', '${firstMenuKey}', '${parentId}', '${M_order_num}', '${path}', '',null,   1, 0, 'M', '0', '0', '', '${M.icon}',   'admin', sysdate(), '', null, '${firstMenuKey}-目录');
+        INSERT INTO sys_menu
+        VALUES ('${M.menuId}', '${firstMenuKey}', '${parentId}', '${M_order_num}', '${path}', '', NULL, 1, 0, 'M', '0',
+                '0', '', '${M.icon}', 'admin', SYSDATE(), '', NULL, '${firstMenuKey}-目录');
     `;
 }
 
@@ -73,7 +80,9 @@ insert into sys_menu values('${M.menuId}', '${firstMenuKey}', '${parentId}', '${
 function secondaryMenu(C, C_order_nume, M, key2) {
     let perms = `${M.path}:${C.path}:list`.replace(/\//g, ':');
     return `
-insert into sys_menu values('${C.menuId}',  '${key2}', '${M.menuId}',   '${C_order_nume}', '${C.path}',       '${M.path}/${C.path}/index', '', 1, 0, 'C', '0', '0', '${perms}', '${C.icon}','admin', sysdate(), '', null, '${key2}-菜单');
+        INSERT INTO sys_menu
+        VALUES ('${C.menuId}', '${key2}', '${M.menuId}', '${C_order_nume}', '${C.path}', '${M.path}/${C.path}/index',
+                '', 1, 0, 'C', '0', '0', '${perms}', '${C.icon}', 'admin', SYSDATE(), '', NULL, '${key2}-菜单');
     `;
 }
 
@@ -83,17 +92,10 @@ insert into sys_menu values('${C.menuId}',  '${key2}', '${M.menuId}',   '${C_ord
 function buttonMenu(F, F_order_num, key, C, M, pageName) {
     let perms = `${M.path}:${C.path}:${F.perms}`.replace(/\//g, ':');
     return `
-insert into sys_menu values('${F.menuId}', '${key}', '${C.menuId}', '${F_order_num}',  '', '','', 1, 0, 'F', '0', '0', '${perms}', '#', 'admin', sysdate(), '', null, '${pageName}-${key}-按钮');
+        INSERT INTO sys_menu
+        VALUES ('${F.menuId}', '${key}', '${C.menuId}', '${F_order_num}', '', '', '', 1, 0, 'F', '0', '0', '${perms}',
+                '#', 'admin', SYSDATE(), '', NULL, '${pageName}-${key}-按钮');
     `;
-
 }
 
-const fs = require('fs-extra')
-
-async function example(f, text) {
-    await fs.outputFile(f, text);
-}
-
-
-example("../sql-mall/mall-menu.sql", sql);
 
---
+++ tools/utils.js
@@ -0,0 +1,26 @@
+const fs = require('fs-extra')
+const menuTomlPath = "./menu/menu.toml";
+
+async function exportFile(f, text) {
+    await fs.outputFile(f, text);
+}
+
+async function outMenuToml(text) {
+    await exportFile(menuTomlPath, text)
+}
+
+async function outSql(text) {
+    await exportFile("./menu.sql", text)
+}
+
+async function reorderMenuIds() {
+    const html = require('fs').readFileSync(menuTomlPath, 'utf8');
+    let id = 3000;
+    const toml = html.replace(/menuId = (\d+)/g, (a, match) => `menuId = ${id++}`);
+    await outMenuToml(toml);
+}
+
+exports.reorderMenuIds = reorderMenuIds;
+exports.exportFile = exportFile;
+exports.outSql = outSql;
+exports.outMenuToml = outMenuToml;
