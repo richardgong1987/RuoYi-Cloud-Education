7b4ded2b46278b2e163e60f03fee47860f1c4f9f
gong.hanjin <richardgong1988@gmail.com>
2021-12-28 21:13:16 +0800

    feat: add education sql

--- sql-eduction/edu2021-12-24.sql
+++ sql-eduction/edu2021-12-24.sql
@@ -1,49 +1,103 @@
-create table edu_management_classes
+DROP TABLE IF EXISTS edu_members_manageclasses;
+CREATE TABLE edu_members_manageclasses
 (
-    id           bigint(20)                             not null auto_increment primary key comment 'id',
-    name         varchar(255) default ''                not null comment '班级名称',
-    school_id    bigint(20)   default 0                 not null comment '所属学校',
-    students_num int          default 0                 not null comment '学生数量',
-    headteacher  bigint       default 0                 not null comment '班主任',
-    phone        varchar(16)  default ''                not null comment '电话',
+    id           BIGINT(20)                             NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    name         VARCHAR(255) DEFAULT ''                NOT NULL COMMENT '班级名称',
+    school_id    BIGINT(20)   DEFAULT 0                 NOT NULL COMMENT '所属学校',
+    students_num INT          DEFAULT 0                 NOT NULL COMMENT '学生数量',
+    headteacher  BIGINT       DEFAULT 0                 NOT NULL COMMENT '班主任',
+    phone        VARCHAR(16)  DEFAULT ''                NOT NULL COMMENT '电话',
 
-    create_time  datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    update_time  datetime     default CURRENT_TIMESTAMP not null comment '更新时间',
-    update_by    varchar(64)  default ''                not null comment '更新者',
-    create_by    varchar(64)  default ''                not null comment '创建者'
+    create_time  DATETIME     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
+    update_time  DATETIME     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '更新时间',
+    update_by    VARCHAR(64)  DEFAULT ''                NOT NULL COMMENT '更新者',
+    create_by    VARCHAR(64)  DEFAULT ''                NOT NULL COMMENT '创建者'
 )
-    comment '班级管理';
+    COMMENT '班级管理';
 
-
-create table edu_management_schools
+DROP TABLE IF EXISTS edu_members_manageschools;
+CREATE TABLE edu_members_manageschools
 (
-    id              bigint(20)                             not null auto_increment primary key comment 'id',
-    name            varchar(255) default ''                not null comment '学校名称',
-    type            int          default 0                 not null comment '学校类型',
-    all_classes_num int          default 0                 not null comment '班级数量',
-    principal_name  varchar(50)  default ''                not null comment '校长姓名',
-    phone           varchar(20)  default ''                not null comment '电话',
+    id              BIGINT(20)                             NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    name            VARCHAR(255) DEFAULT ''                NOT NULL COMMENT '学校名称',
+    type            INT          DEFAULT 0                 NOT NULL COMMENT '学校类型',
+    all_classes_num INT          DEFAULT 0                 NOT NULL COMMENT '班级数量',
+    principal_name  VARCHAR(50)  DEFAULT ''                NOT NULL COMMENT '校长姓名',
+    phone           VARCHAR(20)  DEFAULT ''                NOT NULL COMMENT '电话',
 
-    create_time     datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    update_time     datetime     default CURRENT_TIMESTAMP not null comment '更新时间',
-    update_by       varchar(64)  default ''                not null comment '更新者',
-    create_by       varchar(64)  default ''                not null comment '创建者'
+    create_time     DATETIME     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
+    update_time     DATETIME     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '更新时间',
+    update_by       VARCHAR(64)  DEFAULT ''                NOT NULL COMMENT '更新者',
+    create_by       VARCHAR(64)  DEFAULT ''                NOT NULL COMMENT '创建者'
 )
-    comment '学校管理';
+    COMMENT '学校管理';
 
 
-create table edu_management_students
+DROP TABLE IF EXISTS edu_members_managestudents;
+CREATE TABLE edu_members_managestudents
 (
-    id          bigint(20)                            not null auto_increment primary key comment 'id',
-    name        varchar(50) default ''                not null comment '学生姓名',
-    school_id   bigint(20)  default 0                 not null comment '所属学校',
-    class_id    bigint(20)  default 0                 not null comment '所属班级',
-    is_vip      char        default 'N'               not null comment '是否vip(Y是 N否)',
+    id          BIGINT(20)                            NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    name        VARCHAR(50) DEFAULT ''                NOT NULL COMMENT '学生姓名',
+    school_id   BIGINT(20)  DEFAULT 0                 NOT NULL COMMENT '所属学校',
+    class_id    BIGINT(20)  DEFAULT 0                 NOT NULL COMMENT '所属班级',
+    is_vip      CHAR        DEFAULT 'N'               NOT NULL COMMENT '是否vip(Y是 N否)',
 
-    create_time datetime    default CURRENT_TIMESTAMP not null comment '创建时间',
-    update_time datetime    default CURRENT_TIMESTAMP not null comment '更新时间',
-    update_by   varchar(64) default ''                not null comment '更新者',
-    create_by   varchar(64) default ''                not null comment '创建者'
+    create_time DATETIME    DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
+    update_time DATETIME    DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '更新时间',
+    update_by   VARCHAR(64) DEFAULT ''                NOT NULL COMMENT '更新者',
+    create_by   VARCHAR(64) DEFAULT ''                NOT NULL COMMENT '创建者'
 
 )
-    comment '学生管理';
+    COMMENT '学生管理';
+
+
+
+DROP TABLE IF EXISTS edu_members_managemembers;
+CREATE TABLE edu_members_managemembers
+(
+    id          BIGINT(20)                            NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    name        VARCHAR(50) DEFAULT ''                NOT NULL COMMENT '会员姓名',
+    type        INT         DEFAULT 0                 NOT NULL COMMENT '注册类型',
+    school_id   BIGINT(20)  DEFAULT 0                 NOT NULL COMMENT '所属学校',
+    class_id    BIGINT(20)  DEFAULT 0                 NOT NULL COMMENT '所属班级',
+
+    vip_time    DATETIME                              NULL COMMENT 'VIP时间',
+
+    create_time DATETIME    DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
+    update_time DATETIME    DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '更新时间',
+    update_by   VARCHAR(64) DEFAULT ''                NOT NULL COMMENT '更新者',
+    create_by   VARCHAR(64) DEFAULT ''                NOT NULL COMMENT '创建者'
+
+)
+    COMMENT '会员管理';
+
+
+
+DROP TABLE IF EXISTS edu_teacher_infos;
+CREATE TABLE edu_teacher_infos
+(
+    id                   BIGINT(20)                             NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    name                 VARCHAR(50)  DEFAULT ''                NOT NULL COMMENT '姓名',
+    type                 INT          DEFAULT 0                 NOT NULL COMMENT '类型',
+    school_id            BIGINT(20)   DEFAULT 0                 NOT NULL COMMENT '所属学校',
+    class_id             BIGINT(20)   DEFAULT 0                 NOT NULL COMMENT '所属班级',
+
+    classes_nums         INT          DEFAULT 0                 NOT NULL COMMENT '班级数量',
+
+    phone                VARCHAR(20)  DEFAULT ''                NOT NULL COMMENT '电话',
+
+    birthday             DATETIME     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '生日',
+    entry_time           DATETIME     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '入职时间',
+    education            INT          DEFAULT 0                 NOT NULL COMMENT '学历',
+    graduate_school      VARCHAR(100) DEFAULT ''                NOT NULL COMMENT '毕业院校',
+    certificate_botained VARCHAR(500) DEFAULT ''                NOT NULL COMMENT '所获证书',
+
+    create_time          DATETIME     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
+    update_time          DATETIME     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '更新时间',
+    update_by            VARCHAR(64)  DEFAULT ''                NOT NULL COMMENT '更新者',
+    create_by            VARCHAR(64)  DEFAULT ''                NOT NULL COMMENT '创建者'
+
+)
+    COMMENT '老师信息';
+
+
