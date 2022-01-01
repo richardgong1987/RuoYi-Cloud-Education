7e98593145b694edf04933621894e64a8f273885
gong.hanjin <richardgong1988@gmail.com>
2022-01-01 13:13:01 +0800

    fix: show pages

--- ruoyi-ui/src/store/getters.js
+++ ruoyi-ui/src/store/getters.js
@@ -1,3 +1,4 @@
+import { allColumnList } from '@/libs/map.js'
 const getters = {
   sidebar: state => state.app.sidebar,
   size: state => state.app.size,
@@ -14,5 +15,25 @@ const getters = {
   topbarRouters:state => state.permission.topbarRouters,
   defaultRoutes:state => state.permission.defaultRoutes,
   sidebarRouters:state => state.permission.sidebarRouters,
+
+  // 登录状态
+  isLogin: (state) => state.user.isLogin,
+  // 用户姓名
+  username: (state) => state.user.userInfoObj.username,
+  // 用户ID
+  userId: (state) => state.user.userInfoObj.userId,
+  // 表格显示列
+  selectedColumnList: (state) =>
+    state.fileList.selectedColumnList === null
+      ? allColumnList
+      : state.fileList.selectedColumnList.split(','),
+  // 文件查看模式
+  fileModel: (state) =>
+    state.fileList.fileModel === null ? 0 : Number(state.fileList.fileModel),
+  // 网格模式 & 时间线模式下 文件图标大小
+  gridSize: (state) => state.fileList.gridSize,
+  // 剩余存储空间
+  remainderStorageValue: (state) =>
+    state.sideMenu.totalStorageValue - state.sideMenu.storageValue
 }
 export default getters
--- ruoyi-ui/src/store/index.js
+++ ruoyi-ui/src/store/index.js
@@ -7,6 +7,10 @@ import permission from './modules/permission'
 import settings from './modules/settings'
 import getters from './getters'
 
+import fileList from './modules/fileList' //  文件列表模块
+import sideMenu from './modules/sideMenu' //  左侧菜单模块
+import common from './modules/common' //  公共模块
+
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
@@ -15,7 +19,10 @@ const store = new Vuex.Store({
     user,
     tagsView,
     permission,
-    settings
+    settings,
+    fileList,
+    sideMenu,
+    common
   },
   getters
 })
