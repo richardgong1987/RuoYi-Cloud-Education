5fbbb5ee34394e366efbdf6516eb798a6dbc97e0
gong.hanjin <richardgong1988@gmail.com>
2022-01-02 18:42:47 +0800

    feat: update drive type show

--- ruoyi-modules/ruoyi-file/src/main/java/com/ruoyi/file/mapper/ShareMapper.java
+++ ruoyi-modules/ruoyi-file/src/main/java/com/ruoyi/file/mapper/ShareMapper.java
@@ -5,10 +5,11 @@ import java.util.List;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ruoyi.file.domain.Share;
 import com.ruoyi.file.vo.share.ShareListVO;
+import org.apache.ibatis.annotations.Param;
 
 public interface ShareMapper extends BaseMapper<Share> {
 
-    List<ShareListVO> selectShareList(String shareFilePath, String shareBatchNum, Long beginCount, Long pageCount, Long userId);
+    List<ShareListVO> selectShareList(@Param("shareFilePath") String shareFilePath, String shareBatchNum, @Param("beginCount") Long beginCount, @Param("pageCount") Long pageCount, @Param("userId") long userId);
 
-    int selectShareListTotalCount(String shareFilePath, String shareBatchNum, Long userId);
+    int selectShareListTotalCount(@Param("shareFilePath") String shareFilePath, @Param("shareBatchNum") String shareBatchNum, @Param("userId") long userId);
 }
--- ruoyi-modules/ruoyi-file/src/main/java/com/ruoyi/file/mapper/UserFileMapper.java
+++ ruoyi-modules/ruoyi-file/src/main/java/com/ruoyi/file/mapper/UserFileMapper.java
@@ -12,9 +12,9 @@ public interface UserFileMapper extends BaseMapper<UserFile> {
 
     List<FileListVo> userFileList(@Param("userFile") UserFile userFile, @Param("beginCount") Long beginCount, @Param("pageCount") Long pageCount);
 
-    void updateFilepathByPathAndName(String oldfilePath, String newfilePath, String fileName, String extendName, long userId);
+    void updateFilepathByPathAndName(String oldfilePath, String newfilePath, String fileName, String extendName, @Param("userId") long userId);
 
-    void updateFilepathByFilepath(String oldfilePath, String newfilePath, long userId);
+    void updateFilepathByFilepath(String oldfilePath, String newfilePath, @Param("userId") long userId);
 
     void batchInsertByPathAndName(@Param("oldFilePath") String oldFilePath,
                                   @Param("newFilePath") String newfilePath,
@@ -26,13 +26,13 @@ public interface UserFileMapper extends BaseMapper<UserFile> {
                                @Param("newFilePath") String newfilePath,
                                @Param("userId") long userId);
 
-    List<FileListVo> selectFileByExtendName(List<String> fileNameList, Long beginCount, Long pageCount, long userId);
+    List<FileListVo> selectFileByExtendName(@Param("fileNameList") List<String> fileNameList, @Param("beginCount") Long beginCount, @Param("pageCount") Long pageCount, @Param("userId") long userId);
 
-    Long selectCountByExtendName(List<String> fileNameList, Long beginCount, Long pageCount, long userId);
+    Long selectCountByExtendName(@Param("fileNameList") List<String> fileNameList, @Param("beginCount") Long beginCount, @Param("pageCount") Long pageCount, @Param("userId") long userId);
 
-    List<FileListVo> selectFileNotInExtendNames(List<String> fileNameList, Long beginCount, Long pageCount, long userId);
+    List<FileListVo> selectFileNotInExtendNames(@Param("fileNameList") List<String> fileNameList, @Param("beginCount") Long beginCount, @Param("pageCount") Long pageCount, @Param("userId") long userId);
 
-    Long selectCountNotInExtendNames(List<String> fileNameList, Long beginCount, Long pageCount, long userId);
+    Long selectCountNotInExtendNames(@Param("fileNameList") List<String> fileNameList, @Param("beginCount") Long beginCount, @Param("pageCount") Long pageCount, @Param("userId") long userId);
 
     Long selectStorageSizeByUserId(@Param("userId") Long userId);
 }
