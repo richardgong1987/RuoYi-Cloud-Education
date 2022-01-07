fc7ec9734880ea0ae3040217fd7b06a936a358a5
gong.hanjin <richardgong1988@gmail.com>
2022-01-07 09:03:36 +0800

    feat: push docker hub

--- docker-push.sh
+++ docker-push.sh
@@ -4,3 +4,30 @@ cd docker
 
 sh copy.sh
 
+docker-compose build --no-cache
+
+docker tag docker_ruoyi-mysql richardgong1987/docker_ruoyi-mysql
+docker tag docker_ruoyi-redis richardgong1987/docker_ruoyi-redis
+docker tag docker_ruoyi-nacos richardgong1987/docker_ruoyi-nacos
+docker tag docker_ruoyi-minio richardgong1987/docker_ruoyi-minio
+docker tag docker_ruoyi-elasticsearch richardgong1987/docker_ruoyi-elasticsearch
+
+docker tag docker_ruoyi-gateway richardgong1987/docker_ruoyi-gateway
+docker tag docker_ruoyi-auth richardgong1987/docker_ruoyi-auth
+docker tag docker_ruoyi-modules-system richardgong1987/docker_ruoyi-modules-system
+docker tag docker_ruoyi-modules-file richardgong1987/docker_ruoyi-modules-file
+docker tag docker_ruoyi-modules-education-admin richardgong1987/docker_ruoyi-modules-education-admin
+
+docker login --username=richardgong1987 --password-stdin  < ~/DockerPassword.txt
+
+docker push  richardgong1987/docker_ruoyi-mysql
+docker push  richardgong1987/docker_ruoyi-redis
+docker push  richardgong1987/docker_ruoyi-nacos
+docker push  richardgong1987/docker_ruoyi-minio
+docker push  richardgong1987/docker_ruoyi-elasticsearch
+
+docker push  richardgong1987/docker_ruoyi-gateway
+docker push  richardgong1987/docker_ruoyi-auth
+docker push  richardgong1987/docker_ruoyi-modules-system
+docker push  richardgong1987/docker_ruoyi-modules-file
+docker push  richardgong1987/docker_ruoyi-modules-education-admin
