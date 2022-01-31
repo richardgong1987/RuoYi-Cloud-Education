f66cd92ad868707087c8b9786f1b594887a56d75
gong.hanjin <richardgong1988@gmail.com>
2022-01-31 19:59:42 +0800

    feat:localhost

--- start-jar.sh
+++ start-jar.sh
@@ -7,9 +7,9 @@ nohup java -jar ./ruoyi-gateway/target/ruoyi-gateway.jar &
 
 nohup java -jar ./ruoyi-auth/target/ruoyi-auth.jar &
 
-nohup java -jar ./ruoyi-modules/system/target/ruoyi-modules-system.jar &
+nohup java -jar ./ruoyi-modules/ruoyi-system/target/ruoyi-modules-system.jar &
 
-nohup java -jar ./ruoyi-modules/file/target/ruoyi-modules-file.jar &
+nohup java -jar ./ruoyi-modules/ruoyi-file/target/ruoyi-modules-file.jar &
 
 nohup java -jar ./ruoyi-modules-eduction/education-admin/target/education-admin.jar &
 
